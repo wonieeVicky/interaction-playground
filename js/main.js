fc616211d5ad788faa83defb1ba7fe6c836ca084
Vicky <hwfongfing@gmail.com>
2021-09-16 23:48:52 +0900

    feat: section-2 canvas 태그를 이용한 비디오 스크롤 애니메이션 구현

--- js/main.js
+++ js/main.js
@@ -65,8 +65,15 @@
         messageC: document.querySelector("#scroll-section-2 .c"),
         pinB: document.querySelector("#scroll-section-2 .b .pin"),
         pinC: document.querySelector("#scroll-section-2 .c .pin"),
+        canvas: document.querySelector("#video-canvas-1"),
+        context: document.querySelector("#video-canvas-1").getContext("2d"),
+        videoImages: [], // image sequence 저장
       },
       values: {
+        videoImageCount: 960,
+        imageSequence: [0, 959],
+        canvas_opacity_in: [0, 1, { start: 0, end: 0.1 }],
+        canvas_opacity_out: [1, 0, { start: 0.95, end: 1 }],
         messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
         messageB_translateY_in: [30, 0, { start: 0.5, end: 0.55 }],
         messageC_translateY_in: [30, 0, { start: 0.72, end: 0.77 }],
@@ -107,6 +114,12 @@
       imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
       sceneInfo[0].objs.videoImages.push(imgElem);
     }
+    let imgElem2;
+    for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
+      imgElem2 = new Image(); // === document.createElement('img')
+      imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`;
+      sceneInfo[2].objs.videoImages.push(imgElem2);
+    }
   })();
 
   function setLayout() {
@@ -132,6 +145,7 @@
     document.body.setAttribute("id", `show-scene-${currentScene}`);
     const heightRatio = window.innerHeight / 1080; // canvas 원래 크기 대비 현재 디바이스 height의 비율
     sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
+    sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
   }
 
   function calcValues(values, currentYOffset) {
@@ -243,6 +257,16 @@
         break;
       case 2:
         // console.log(2);
+        let sequence2 = Math.round(calcValues(values.imageSequence, currentYOffset)); // 스크롤에 따른 0 - 299
+        objs.context.drawImage(objs.videoImages[sequence2], 0, 0);
+        if (scrollRatio <= 0.5) {
+          // in
+          objs.canvas.style.opacity = calcValues(values.canvas_opacity_in, currentYOffset);
+        } else {
+          // out
+          objs.canvas.style.opacity = calcValues(values.canvas_opacity_out, currentYOffset);
+        }
+
         if (scrollRatio <= 0.25) {
           // in
           objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
